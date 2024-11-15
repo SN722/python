f6ece51296d9e4fe37eaeb7089cdb9fddc8d62e0
SN722 <nazneen7227@gmail.com>
2024-11-16 01:41:49 +0530

    up

---
+++ oprtr.py
@@ -0,0 +1,52 @@
+# operators in python 
+# 1.Arithmetic operators:
+print("Arithmetic operators")
+print(20 + 10)
+print(10 * 20)
+print(10 - 5)
+print(10 % 5)
+print(10 / 5)
+print(15 // 2)
+print(10 ** 10)
+
+# 2.Assignment operators:
+print("Assignment operators")
+x = 5
+print(x)
+
+y = 5
+y += 10
+print(y)
+
+z = 10
+z -= 5
+print(z)
+
+a = 10
+a *= 5
+print(a)
+
+b = 10
+b /= 5
+print(b)
+
+c = 10
+c %= 5
+print(c)
+
+d = 10
+d //= 5
+print(d)
+
+e = 10
+e **= 5
+print(e)
+
+f = 10
+f &= 10
+print(f)
+
+g = 10 
+g |= 5
+print(g)
+
